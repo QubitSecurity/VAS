8ae66a2564be44de0b001d98b92f396def7df090
Qubit Security Inc. <plura@qubitsec.com>
2026-04-01 18:44:20 +0900

    Update and rename check-deny-7.ps1 to check-deny-7-general-work-tools.ps1

--- windows/validation/wdac/check-deny-7-general-work-tools.ps1
+++ windows/validation/wdac/check-deny-7-general-work-tools.ps1
@@ -1,16 +1,14 @@
-﻿# 테스트할 실행 파일 목록
 $targetExes = @(
-    "cmdl32.exe",
-    "colorcpl.exe",
-    "devicecredentialdeployment.exe",
-    "finger.exe",
-    "ftp.exe",
-    "msdt.exe",
-    "sftp.exe",
-    "wab.exe",
-    "winfile.exe",
-    "wlrmdr.exe",
-    "wmic.exe"
+    "appinstaller.exe",
+    "desktopimgdownldr.exe",
+    "imewdbld.exe",
+    "msedge.exe",
+    "msedge_proxy.exe",
+    "msedgewebview2.exe",
+    "onedrivestandaloneupdater.exe",
+    "winget.exe",
+    "workfolders.exe",
+    "xwizard.exe"
 )
 
 Write-Host "WDAC 실행 차단 테스트를 시작합니다..." -ForegroundColor Cyan
